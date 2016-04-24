7756c8f1e62a8ef476b17d373f91cb2f06a73871
Sander Ronde <awsdfgvhbjn@gmail.com>
2016-04-24 03:03:47 +0200

    Added the main moveWebsite task. Moved gruntfile to the root. Changed gitignore to ignore all irrelevant files

---
+++ Gruntfile.js
@@ -0,0 +1,17 @@
+﻿module.exports = function(grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		copy: {
+			build: {
+				files: [
+					{ expand: true, cwd: 'Custom Right-Click Menu/app/build/website/', src: ['./**'], dest: './' }
+				]
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-copy');
+
+	//Moves the website to the main directory
+	grunt.registerTask('moveWebsite', ['copy']);
+}
